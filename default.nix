6d5c5860e76630bd44ec6f74589a51622e6d19a0
kon <kon@kon.ninja>
2021-09-07 11:28:58 +0200

    update default.nix

--- default.nix
+++ default.nix
@@ -11,5 +11,16 @@ pkgs.buildGoModule rec {
   vendorSha256 = "0pc40jfmv3svnbycj8028n8hgwq2x9hidgv048snl4w4g7ydh79f";
 
   subPackages = ["cmd/lncli" "cmd/lnd"];
+  preBuild = let
+      buildVars = {
+        RawTags = pkgs.lib.concatStringsSep "," tags;
+        GoVersion = "$(go version | egrep -o 'go[0-9]+[.][^ ]*')";
+      };
+      buildVarsFlags = pkgs.lib.concatStringsSep " " (pkgs.lib.mapAttrsToList (k: v: "-X github.com/lightningnetwork/lnd/build.${k}=${v}") buildVars);
+    in
+    pkgs.lib.optionalString (tags != []) ''
+      buildFlagsArray+=("-tags=${pkgs.lib.concatStringsSep " " tags}")
+      buildFlagsArray+=("-ldflags=${buildVarsFlags}")
+    '';
 
 }
